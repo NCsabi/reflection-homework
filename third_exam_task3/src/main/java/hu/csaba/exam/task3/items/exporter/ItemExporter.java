ace8dd87a11305603c78b8a214a5d74ed8864a8e
NCsabi <123809434+NCsabi@users.noreply.github.com>
2025-12-29 18:38:17 +0100

    Delete third_exam_task3 directory

--- third_exam_task3/src/main/java/hu/csaba/exam/task3/items/exporter/ItemExporter.java
+++
@@ -1,9 +0,0 @@
-package hu.csaba.exam.task3.items.exporter;
-
-import hu.csaba.exam.task3.items.model.Item;
-
-import java.util.List;
-
-public interface ItemExporter {
-    void export(List<Item> items) throws Exception;
-}
